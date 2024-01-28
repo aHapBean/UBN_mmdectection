9960fa8a636d271408f04f71a9eb1f8222cf6889
aHapBean <2827276973@qq.com>
2024-01-28 20:06:31 +0800

    update

--- run-seq.sh
+++ run-seq.sh
@@ -1,25 +1,29 @@
 # bash ./tools/dist_train.sh ./configs/a_faster_rcnn/bn_faster-rcnn_r50_fpn_1x_coco.py 8
 
-# dect   resnet101
-# bash ./tools/dist_train.sh ./configs/a_faster_rcnn/ubn_faster-rcnn_r101_fpn_1x_coco.py 8
-
-# bash ./tools/dist_train.sh ./configs/a_faster_rcnn/bn_faster-rcnn_r101_fpn_1x_coco.py 8
 
 
-# seg    resnet50
+TODO:
+
+# dect   resnet101    jxs2在跑这个
+bash ./tools/dist_train.sh ./configs/a_faster_rcnn/ubn_faster-rcnn_r101_fpn_1x_coco.py 8
+
+bash ./tools/dist_train.sh ./configs/a_faster_rcnn/bn_faster-rcnn_r101_fpn_1x_coco.py 8
+
+
+# seg    resnet50    
+# 53000 在跑这个
 bash ./tools/dist_train.sh ./configs/a_mask_rcnn/bn_mask-rcnn_r50_fpn_1x_coco.py 8
-
+# pj2 跑这个
 bash ./tools/dist_train.sh ./configs/a_mask_rcnn/ubn_mask-rcnn_r50_fpn_1x_coco.py 8
 
 
 # seg    resnet101
-bash ./tools/dist_train.sh ./configs/a_mask_rcnn/bn_mask-rcnn_r101_fpn_1x_coco.py 1
 
-bash ./tools/dist_train.sh ./configs/a_mask_rcnn/ubn_mask-rcnn_r101_fpn_1x_coco.py 1
+# 这个bn要不七卡跑完 TODO 
+bash ./tools/dist_train.sh ./configs/a_mask_rcnn/bn_mask-rcnn_r101_fpn_1x_coco.py 7
+# TODO pj2 跑这个
+bash ./tools/dist_train.sh ./configs/a_mask_rcnn/ubn_mask-rcnn_r101_fpn_1x_coco.py 8
 
 # ubn mask rcnn
 
-# 如果有空余时间考虑跑 resnet101 的实验
-
-# 1.27这是 tmux 00 53000 正在跑8卡 
-# ubn faster rcnn r50 √
+# 貌似mask 也是 12 epochs 因为是 1x
